0f4d148bbe43b6339f9736d6932b5c142e3a342c
Jason <jason@glamdring.local>
2013-11-25 11:17:05 -0700

    More CSS fixes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   def self.author(title, body)
     friendly = title.parameterize.underscore
-    body = body.gsub /photo:([0-9]*)[^0-9]?/, '</p><p><a href="/photo/\1/img.jpg"><img src="/photo/\1/detail_img.jpg"></a></p><p>'
+    body = body.gsub /photo:([0-9]*)[^0-9]?/, '</p><p><div class="photo"><a href="/photo/\1/img.jpg"><img src="/photo/\1/detail_img.jpg"></a></div></p><p>'
     body = "<p>#{body}</p>".gsub /<p>\s*<\/p>/, ''
     post = Post.new(title: title, friendly: friendly, body: body)
     post.save!
