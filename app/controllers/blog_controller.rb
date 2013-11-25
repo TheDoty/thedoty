3649378a1f4a8495c0db7c1c2c3ca735d0d726c9
Jason <jason@glamdring.local>
2013-11-25 11:37:47 -0700

    Pagination of blog posts

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,6 +1,6 @@
 class BlogController < ApplicationController
   def index
-    @posts = Post.paginate(:page => params[:page]).order(:created_at).reverse
+    @posts = Post.paginate(:page => params[:page]).order(:created_at).reverse_order
   end
 
   def show
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,17 @@
 class Post < ActiveRecord::Base
-  def self.author(title, body)
+  self.per_page = 5
+
+  def self.author(title, body, created_at = nil)
     friendly = title.parameterize.underscore
     body = body.gsub /photo:([0-9]*)[^0-9]?/, '</p><p><div class="photo"><a href="/photo/\1/img.jpg"><img src="/photo/\1/detail_img.jpg"></a></div></p><p>'
     body = "<p>#{body}</p>".gsub /<p>\s*<\/p>/, ''
+
     post = Post.new(title: title, friendly: friendly, body: body)
     post.save!
+
+    if created_at
+      post.created_at = created_at
+      post.save!
+    end
   end
 end
