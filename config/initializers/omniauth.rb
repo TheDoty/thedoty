9aa1ec50dcd218cfdf9c778f0be2d30b85c8fd21
Jason <jason@glamdring.local>
2013-12-18 21:21:25 -0700

    Copy and paste error :|

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :twitter, 'HcI9zaJYcMTAKB6mSxjLQ', 'HPrhCOeA3uLGvOGBj2mj9zK06knlFFumLVmhb3nR75' # Please don't steal me :)  For a real application, these would have to be stored on the production server and not in source control.
+  provider :twitter, 'HcI9zaJYcMTAKB6mSxjLQ', 'HPrhCOeA3uLGvOGBj2mj9zK06knlFFumLVmhb3nR75s' # Please don't steal me :)  For a real application, these would have to be stored on the production server and not in source control.
 end
