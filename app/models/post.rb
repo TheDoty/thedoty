7b6b01c244aa0c4ab9578d2469a13733390336df
The Doty <iamthedoty@gmail.com>
2013-12-29 16:17:56 -0700

    Fix for image link in blog authoring

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
 
   def self.author(title, body, created_at = nil)
     friendly = title.parameterize.underscore
-    body = body.gsub /photo:([0-9]*)[^0-9]?/, '</p><p><div class="photo"><a href="/photo/\1/img.jpg"><img src="/photo/\1/detail_img.jpg"></a></div></p><p>'
+    body = body.gsub /photo:([0-9]*)[^0-9]?/, '</p><p><div class="photo"><a href="/photos/\1"><img src="/photo/\1/detail_img.jpg"></a></div></p><p>'
     body = "<p>#{body}</p>".gsub /<p>\s*<\/p>/, ''
 
     post = Post.new(title: title, friendly: friendly, body: body)
